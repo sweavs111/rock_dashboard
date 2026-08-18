98bf3013bbe0cba78f3e5a30f910628ab217a89d
sweavs111 <seth.weaver111@gmail.com>
2026-08-18 12:17:28 -0400

    control plug / unplug

--- dashboard/app.py
+++ dashboard/app.py
@@ -24,6 +24,8 @@ class Dashboard:
         
         # setup controller support
         pygame.joystick.init()
+        # Keep track of connected joysticks
+        joysticks = {}
 
         # set up progress bar
         self.progress_bar = progress_bar.ProgressBar()
@@ -39,12 +41,20 @@ class Dashboard:
                 if event.type == pygame.QUIT:
                     running = False
 
-                elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.type == pygame.MOUSEBUTTONDOWN:
                     if event.button == 1:  # 1 represents the left mouse button
                         index = self.panel.detect_click(event)
                         if index is not None:
                             prog_bars[index] = (pygame.time.get_ticks(), datetime.now(ZoneInfo("America/New_York")))
 
+                # xbox controller plugged in
+                if event.type == pygame.JOYDEVICEADDED:
+                    xbox = pygame.joystick.Joystick(event.device)
+                    xbox.init()
+                    joysticks[xbox.get_instance_id()] = xbox
+                # xbox controller unplugged
+                if event.type == pygame.JOYDEVICEREMOVED:
+                    del joysticks[event.instance_id()]
 
             # fill the screen with a color to wipe away anything from last frame
             self.screen.fill(settings.GREY)
